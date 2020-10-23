55d2fb0f1d7203016094a1628aa3891588d184ed
VikaGor <Vica-gor@mail.ru>
2020-10-23 23:34:18 +0300

    Initialize repository

---
+++ lib/vikagor_palindrome.rb
@@ -0,0 +1,6 @@
+require "vikagor_palindrome/version"
+
+module VikagorPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/vikagor_palindrome/version.rb
@@ -0,0 +1,3 @@
+module VikagorPalindrome
+  VERSION = "0.1.0"
+end
